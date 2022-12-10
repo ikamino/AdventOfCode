678333e60bc99418e7a45312c9ec295fff44844c
ikamino <elee1820@gmail.com>
2022-12-09 16:32:36 -0800

    day 1 part 2

--- day_1/day1.py
+++ day_1/day1.py
@@ -11,13 +11,23 @@ with open('input.txt') as f:
             d1.append([])
 
 
+# def main(nums):
+#     res = 0
+#     curr = 0
+#     for i in nums: 
+#         curr = sum(i)
+#         res = max(curr, res)
+#     return res
+
+
+
 def main(nums):
-    res = 0
-    curr = 0
-    for i in nums: 
-        curr = sum(i)
-        res = max(curr, res)
-    return res
+
+    for i in range(0,len(nums)): 
+        nums[i] = sum(nums[i])
+
+    nums.sort(reverse = True)
+    return nums[:3]
         
 print(main(d1))
 
